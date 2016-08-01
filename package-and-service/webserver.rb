b9ccec76ff0397dcefe413cef7b5dbc9d30ef1e2
Mike OBrien <miobrien@adobe.com>
2016-08-01 12:32:52 -0700

    add service and homepage

--- package-and-service/webserver.rb
+++ package-and-service/webserver.rb
@@ -4,3 +4,16 @@ apt_update 'Update the apt cache daily' do
 end
 
 package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
